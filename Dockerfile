8ab26f31f08f4b6d8b6602d2e91d853e62ec6a2a
Ramil <galimovr86@gmail.com>
2024-10-31 20:04:50 +0100

    add docker compose

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["RvcApp/RvcApp.csproj", "RvcApp/"]
+RUN dotnet restore "RvcApp/RvcApp.csproj"
+COPY . .
+WORKDIR "/src/RvcApp"
+RUN dotnet build "RvcApp.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "RvcApp.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "RvcApp.dll"]
--- RvcApp/Program.cs
+++ RvcApp/Program.cs
@@ -7,7 +7,6 @@ using RvcApp.Repositories;
 using RvcApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://localhost:5000");
 
 builder.Services.AddControllers(options =>
     {
@@ -19,13 +18,19 @@ builder.Services.AddControllers(options =>
             allowIntegerValues: false));
     });
 
-
 builder.Services.AddScoped<ICleaner, RvcCleaner>();
 builder.Services.AddScoped<IRvcService, RvcService>();
 builder.Services.AddScoped<IRepository, ExecutionRepository>();
 
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
